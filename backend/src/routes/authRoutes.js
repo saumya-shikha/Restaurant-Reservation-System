f7785b3aaf4dad0c94985616a2760456cc7e973a
saumya-shikha <saumyashkh@gmail.com>
2026-07-11 03:44:49 +0530

    Fix folder casing to Capital C

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { body } = require('express-validator');
-const authController = require('../controllers/authController');
+const authController = require('../Controllers/authController');
 const protect = require('../middleware/auth');
 const validate = require('../middleware/validate');
 
